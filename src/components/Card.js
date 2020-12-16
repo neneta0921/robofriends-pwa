62fc38bda8537264f02717080dca4943ada9c267
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-16 22:57:11 +0900

    little improve

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,8 @@ const Card = ({ name, email, id }) => {
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
       <img alt='nekos' src={`https://robohash.org/${id}?size=150x150&set=set4`} />
       <div>
-        <h3 className='helvetica'>{name}</h3>
-        <p className='helvetica'>{email}</p>
+        <h3>{name}</h3>
+        <p>{email}</p>
       </div>
     </div>
   );
